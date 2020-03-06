69c2b7cec2b0194f121a51e1827350d48582315d
rogear2008 <rogear@qq.com>
2020-03-06 13:42:15 +0800

    update data

--- ribbon-service/src/main/java/com/rogear/cloud/ribbonservice/pojo/CommonResult.java
+++ ribbon-service/src/main/java/com/rogear/cloud/ribbonservice/pojo/CommonResult.java
@@ -8,14 +8,14 @@ import java.io.Serializable;
 public class CommonResult<T> implements Serializable {
 
     private static final long serialVersionUID = -1082980142811730082L;
-    private T dta;
+    private T data;
     private String message;
     private Integer code;
 
     public CommonResult(){}
 
-    public CommonResult(T dta, String message, Integer code) {
-        this.dta = dta;
+    public CommonResult(T data, String message, Integer code) {
+        this.data = data;
         this.message = message;
         this.code = code;
     }
@@ -29,12 +29,12 @@ public class CommonResult<T> implements Serializable {
         this(data,"操作成功",200);
     }
 
-    public T getDta() {
-        return dta;
+    public T getData() {
+        return data;
     }
 
-    public void setDta(T dta) {
-        this.dta = dta;
+    public void setData(T data) {
+        this.data = data;
     }
 
     public String getMessage() {
